e3dab9aafff309e4bf38411f5e7f5127a2f27a36
dlswns2480 <dlswns2480@naver.com>
2023-11-30 00:49:42 +0900

    feat : JWT 관련 ErrorCode 추가

--- src/main/java/com/devgym/gymmanager/exception/ErrorCode.java
+++ src/main/java/com/devgym/gymmanager/exception/ErrorCode.java
@@ -8,6 +8,9 @@ import lombok.RequiredArgsConstructor;
 public enum ErrorCode {
     NOT_EXIST_MEMBER("존재하지 않는 회원입니다"),
     NOT_EXIST_TRAINER("존재하지 않는 트레이너 입니다"),
-    NOT_EXIST_ITEM("존재하지 않는 아이템입니다");
+    NOT_EXIST_ITEM("존재하지 않는 아이템입니다"),
+    EXPIRED_TOKEN("이미 만료된 토큰입니다."),
+    NOT_EXIST_TOKEN("토큰이 비었습니다"),
+    NOT_BEARER_TOKEN("Bearer 토큰이 아닙니다.");
     private final String message;
 }
