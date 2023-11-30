9c9610c240505371ec14fe9975cb7b8d41f18ddd
dlswns2480 <dlswns2480@naver.com>
2023-11-30 14:36:13 +0900

    feat : 회원가입 DTO 생성

--- src/main/java/com/devgym/gymmanager/auth/dto/SignUpRequest.java
+++ src/main/java/com/devgym/gymmanager/auth/dto/SignUpRequest.java
@@ -1,4 +1,6 @@
 package com.devgym.gymmanager.auth.dto;
 
-public record SignUpRequest(String memberName, String passWord) {
+import com.devgym.gymmanager.domain.type.Membership;
+
+public record SignUpRequest(String memberName, String passWord, String phoneNumber, Membership membership) {
 }
