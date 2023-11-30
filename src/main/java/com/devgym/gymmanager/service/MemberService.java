ea49b57fccefff14e97b1007e64c83d755a37b6e
dlswns2480 <dlswns2480@naver.com>
2023-11-30 14:36:45 +0900

    feat : MemberService 로그인 로직 구현

--- src/main/java/com/devgym/gymmanager/service/MemberService.java
+++ src/main/java/com/devgym/gymmanager/service/MemberService.java
@@ -1,36 +1,57 @@
 package com.devgym.gymmanager.service;
 
+import com.devgym.gymmanager.auth.dto.LoginRequest;
+import com.devgym.gymmanager.auth.dto.SignUpRequest;
+import com.devgym.gymmanager.auth.utils.JwtUtil;
 import com.devgym.gymmanager.domain.entity.Member;
 import com.devgym.gymmanager.domain.entity.Trainer;
 import com.devgym.gymmanager.domain.type.Membership;
 import com.devgym.gymmanager.dto.request.AddTrainer;
-import com.devgym.gymmanager.dto.request.MemberRequest;
 import com.devgym.gymmanager.dto.response.MemberResponse;
 import com.devgym.gymmanager.dto.response.TrainerResponse;
 import com.devgym.gymmanager.exception.CustomException;
+import com.devgym.gymmanager.exception.ErrorCode;
 import com.devgym.gymmanager.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
-import static com.devgym.gymmanager.exception.ErrorCode.*;
+import static com.devgym.gymmanager.exception.ErrorCode.INVALID_PASSWORD;
+import static com.devgym.gymmanager.exception.ErrorCode.NOT_EXIST_MEMBER;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class MemberService {
+    private final BCryptPasswordEncoder encoder;
+    @Value("${jwt.secret}")
+    private String secretKey;
+    private Long expiredMs = 1000* 60 * 60L;
     private final MemberRepository memberRepository;
     private final TrainerService trainerService;
-
     @Transactional
-    public MemberResponse createMember(MemberRequest request){
+    public MemberResponse signUp(SignUpRequest request){
         validateDuplicate(request); // 중복검증
-        Member member = Member.createMember(request); // 회원 생성
-        Member result = memberRepository.save(member); // 회원 저장
-        return new MemberResponse(result.getName(), result.getMembership());
+        String encodedPassWd = encoder.encode(request.passWord());
+        SignUpRequest signUpRequest = new SignUpRequest(request.memberName(), encodedPassWd, request.phoneNumber(), request.membership());
+        Member member = Member.createMember(signUpRequest); // 회원 생성
+        Member savedMember = memberRepository.save(member); // 회원 저장
+        return new MemberResponse(savedMember.getName(), savedMember.getMembership());
+    }
+    @Transactional
+    public String signIn(LoginRequest request) {
+        String name = request.memberName();
+        Member member = memberRepository.findByName(name).orElseThrow(() -> new CustomException(ErrorCode.NOT_EXIST_MEMBER));
+        if(!member.getPassWord().equals(request.passWord())){
+            throw new CustomException(INVALID_PASSWORD);
+        }
+
+        return JwtUtil.createJwt(request.memberName(), secretKey, expiredMs);
     }
     @Transactional
     public TrainerResponse registerTrainer(AddTrainer request) {
@@ -51,7 +72,7 @@ public class MemberService {
                 .map(member -> new MemberResponse(member.getName(), member.getMembership()))
                 .toList();
     }
-    private void validateDuplicate(MemberRequest request){
+    private void validateDuplicate(SignUpRequest request){
         Optional<Member> member = memberRepository.findByPhoneNumber(request.phoneNumber());
         if(member.isPresent()){
             throw new IllegalStateException("이미 존재하는 회원입니다");
