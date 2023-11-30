761862302c8f2d6c128c6f1c83f653fa7b88433c
dlswns2480 <dlswns2480@naver.com>
2023-11-30 14:34:06 +0900

    feat : 비밀번호 불일치 에러코드 추가

--- src/main/java/com/devgym/gymmanager/exception/ErrorCode.java
+++ src/main/java/com/devgym/gymmanager/exception/ErrorCode.java
@@ -11,6 +11,7 @@ public enum ErrorCode {
     NOT_EXIST_ITEM("존재하지 않는 아이템입니다"),
     EXPIRED_TOKEN("이미 만료된 토큰입니다."),
     NOT_EXIST_TOKEN("토큰이 비었습니다"),
-    NOT_BEARER_TOKEN("Bearer 토큰이 아닙니다.");
+    NOT_BEARER_TOKEN("Bearer 토큰이 아닙니다."),
+    INVALID_PASSWORD("비밀번호가 일치하지 않습니다");
     private final String message;
 }
