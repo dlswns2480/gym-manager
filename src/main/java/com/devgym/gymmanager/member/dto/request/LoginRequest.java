d11aff36232be5f481b141f80df2151d18358aab
dlswns2480 <dlswns2480@naver.com>
2023-12-01 15:34:21 +0900

    fix : 로그인 dto 유효성 검증

--- src/main/java/com/devgym/gymmanager/member/dto/request/LoginRequest.java
+++ src/main/java/com/devgym/gymmanager/member/dto/request/LoginRequest.java
@@ -1,4 +1,10 @@
 package com.devgym.gymmanager.member.dto.request;
 
-public record LoginRequest(String memberName, String passWord) {
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest(
+        @NotBlank(message = "아이디 입력은 필수입니다.")
+        String memberName,
+        @NotBlank(message = "비밀번호 입력은 필수입니다.")
+        String passWord) {
 }
--- src/main/java/com/devgym/gymmanager/trainer/dto/request/AddTrainer.java
+++ src/main/java/com/devgym/gymmanager/trainer/dto/request/AddTrainer.java
@@ -1,4 +1,10 @@
 package com.devgym.gymmanager.trainer.dto.request;
 
-public record AddTrainer(Long memberId, Long trainerId) {
+import jakarta.validation.constraints.NotBlank;
+
+public record AddTrainer(
+        @NotBlank(message = "회원 입력은 필수입니다.")
+        Long memberId,
+        @NotBlank(message = "트레이너 입력은 필수입니다.")
+        Long trainerId) {
 }
