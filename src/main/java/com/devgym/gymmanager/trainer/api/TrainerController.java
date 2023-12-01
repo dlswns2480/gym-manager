0c1459db26755d5512fe476ad7b39a8b4821fe50
dlswns2480 <dlswns2480@naver.com>
2023-12-01 15:32:43 +0900

    feat : Trainer dto 유효성 검증

--- src/main/java/com/devgym/gymmanager/trainer/api/TrainerController.java
+++ src/main/java/com/devgym/gymmanager/trainer/api/TrainerController.java
@@ -1,10 +1,11 @@
 package com.devgym.gymmanager.trainer.api;
 
+import com.devgym.gymmanager.trainer.application.TrainerService;
 import com.devgym.gymmanager.trainer.dto.request.TrainerRequest;
 import com.devgym.gymmanager.trainer.dto.response.TrainerResponse;
-import com.devgym.gymmanager.trainer.application.TrainerService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -22,7 +23,7 @@ public class TrainerController {
     private final TrainerService trainerService;
     @Operation(summary = "트레이너 생성 요청", description = "트레이너 정보가 생성됩니다.", tags = { "TrainerController" })
     @PostMapping("/create")
-    public ResponseEntity<TrainerResponse> createTrainer(Authentication authentication, @RequestBody TrainerRequest request) {
+    public ResponseEntity<TrainerResponse> createTrainer(Authentication authentication, @Valid @RequestBody TrainerRequest request) {
         log.info("{}님이 등록한 트레이너입니다.", authentication.getName());
         return new ResponseEntity<>(trainerService.createTrainer(request), HttpStatus.OK);
     }
--- src/main/java/com/devgym/gymmanager/trainer/dto/request/TrainerRequest.java
+++ src/main/java/com/devgym/gymmanager/trainer/dto/request/TrainerRequest.java
@@ -1,4 +1,16 @@
 package com.devgym.gymmanager.trainer.dto.request;
 
-public record TrainerRequest(String name, String phoneNumber, int career, int hourlyPrice) {
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record TrainerRequest(
+        @NotBlank(message = "이름 기입은 필수입니다.")
+        String name,
+        @Pattern(regexp = "^\\s*(?:\\+?(\\d{1,3}))?([-. (]*(\\d{3})[-. )]*)?((\\d{3})[-. ]*(\\d{2,4})(?:[-.x ]*(\\d+))?)\\s*$", message = "전화번호 형식이 올바르지 않습니다.")
+        String phoneNumber,
+        @Min(value = 0, message = "경력은 1년 이상이어야 합니다.")
+        int career,
+        @Min(value = 0, message = "가격이 0원이하일 수 없습니다.")
+        int hourlyPrice) {
 }
