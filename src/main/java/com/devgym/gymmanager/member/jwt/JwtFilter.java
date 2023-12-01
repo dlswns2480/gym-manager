f69e4b8f1120285dcd5baa86c91526be17b56bac
dlswns2480 <dlswns2480@naver.com>
2023-12-02 01:02:54 +0900

    feat : 리프레시 토큰 검증 로직 추가

--- src/main/java/com/devgym/gymmanager/member/jwt/JwtFilter.java
+++ src/main/java/com/devgym/gymmanager/member/jwt/JwtFilter.java
@@ -18,13 +18,16 @@ import java.util.List;
 
 import static com.devgym.gymmanager.common.exception.ErrorCode.*;
 
-@RequiredArgsConstructor
+
 @Slf4j
+@RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
     private final String secretKey;
+    private final String refreshSecretKey;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
+        String type = request.getHeader("Token-Type");
         if(authorization == null){
             log.info(NOT_EXIST_TOKEN.getMessage());
             filterChain.doFilter(request, response);
@@ -37,12 +40,26 @@ public class JwtFilter extends OncePerRequestFilter {
         }
         String token = authorization.split(" ")[1];
 
-        if(JwtUtil.isExpired(token, secretKey)){ //토큰이 만료된 경우,
-            log.info(EXPIRED_TOKEN.getMessage());
-            filterChain.doFilter(request, response);
-            return;
+        String memberName = "";
+        if(type.equals("access")){
+            log.info("In ACCESS");
+            if(JwtUtil.isExpired(token, secretKey)){ //토큰이 만료된 경우,
+                log.info(EXPIRED_TOKEN.getMessage());
+                filterChain.doFilter(request, response);
+                return;
+            }
+            memberName = JwtUtil.getMemberName(token, secretKey);
         }
-        String memberName = JwtUtil.getMemberName(token, secretKey);
+        else if(type.equals("refresh")){
+            log.info("In REFRESH");
+            if(JwtUtil.isExpired(token, refreshSecretKey)){ //토큰이 만료된 경우,
+                log.info(EXPIRED_TOKEN.getMessage());
+                filterChain.doFilter(request, response);
+                return;
+            }
+            memberName = JwtUtil.getMemberName(token, refreshSecretKey);
+        }
+
         UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(memberName, null, List.of(new SimpleGrantedAuthority("USER")));
 
         authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
