fe7395d54c6c841797bdade9ba24a2d293ef5ad1
dlswns2480 <dlswns2480@naver.com>
2023-12-02 01:02:29 +0900

    feat : 리프레시 토큰 생성 메서드 추가

--- src/main/java/com/devgym/gymmanager/member/jwt/JwtUtil.java
+++ src/main/java/com/devgym/gymmanager/member/jwt/JwtUtil.java
@@ -8,22 +8,35 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.Date;
 @Slf4j
 public class JwtUtil {
-    public static String createJwt(String memberName, String secretKey, Long expireMs){
+
+    public static String createAccessToken(String memberName, String secretKey, Long accessExpiredMs){
         Claims claims = Jwts.claims();
         claims.put("memberName", memberName);
         return Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + expireMs))
+                .setExpiration(new Date(System.currentTimeMillis() + accessExpiredMs))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+    public static String createRefreshToekn(String memberName, String secretKey, Long refreshExpiredMs){
+        Claims claims = Jwts.claims();
+        claims.put("memberName", memberName);
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + refreshExpiredMs))
                 .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
     }
 
     public static boolean isExpired(String token, String secretKey) {
+        log.info(secretKey);
         return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token)
                 .getBody().getExpiration().before(new Date());
     }
     public static String getMemberName(String token, String secretKey){
+        log.info("secret key : {}", secretKey);
         return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token)
                 .getBody().get("memberName", String.class);
     }
