668e0bada0db785cc12af072165fa73445714c74
dlswns2480 <dlswns2480@naver.com>
2023-12-11 23:58:01 +0900

    feat : ItemService 추가

--- src/main/java/com/devgym/gymmanager/item/application/ItemService.java
+++ src/main/java/com/devgym/gymmanager/item/application/ItemService.java
@@ -4,7 +4,7 @@ import com.devgym.gymmanager.item.domain.Item;
 import com.devgym.gymmanager.item.dto.mapper.ItemMapper;
 import com.devgym.gymmanager.item.dto.request.ItemRequest;
 import com.devgym.gymmanager.item.dto.response.ItemResponse;
-import com.devgym.gymmanager.item.repository.ItemRepository;
+import com.devgym.gymmanager.item.repository.ItemsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,15 +17,15 @@ import static com.devgym.gymmanager.item.dto.mapper.ItemMapper.*;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class ItemService {
-    private final ItemRepository itemRepository;
+    private final ItemsRepository itemsRepository;
 
     public ItemResponse createItem(ItemRequest request){
         Item item = new Item(request.category(), request.productName(), request.price(), request.stock());
-        Item savedItem = itemRepository.save(item);
+        Item savedItem = itemsRepository.save(item);
         return toItemOrderResponse(savedItem);
     }
     public List<ItemResponse> findAll(){
-        List<Item> all = itemRepository.findAll();
+        List<Item> all = itemsRepository.findAll();
         return all.stream()
                 .map(ItemMapper::toItemOrderResponse)
                 .toList();
