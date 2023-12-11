f1c1d323dbbc926bd294a95416635ab080d63a15
dlswns2480 <dlswns2480@naver.com>
2023-12-11 23:54:31 +0900

    feat : ItemRepository 추가

--- src/main/java/com/devgym/gymmanager/orderitem/repository/ItemRepository.java
+++ src/main/java/com/devgym/gymmanager/orderitem/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.devgym.gymmanager.orderitem.repository;
 
 import com.devgym.gymmanager.orderitem.domain.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemRepository extends JpaRepository<OrderItem, Long> {
 }
